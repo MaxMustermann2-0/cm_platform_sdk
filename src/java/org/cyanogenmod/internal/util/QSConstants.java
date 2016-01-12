ad9372abfc5f0ca6bd6f90d0dce68fe97f538601
MaxMustermann2.0 <maxmustermannbsp@gmail.com>
2016-01-12 18:13:45 +0100

    Add night mode tile
    Change-Id: Iade4aaec2bb3be283e459a17ee451afe906b6ea7

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -54,6 +54,7 @@ public class QSConstants {
     public static final String TILE_BATTERY_SAVER = "battery_saver";
     public static final String TILE_EDIT = "edit";
     public static final String TILE_DND = "dnd";
+    public static final String TILE_NIGHTMODE = "night_mode";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -96,6 +97,7 @@ public class QSConstants {
 //        STATIC_TILES_AVAILABLE.add(TILE_HEADS_UP);
         STATIC_TILES_AVAILABLE.add(TILE_AMBIENT_DISPLAY);
         STATIC_TILES_AVAILABLE.add(TILE_SYNC);
+        STATIC_TILES_AVAILABLE.add(TILE_NIGHTMODE);
 //        STATIC_TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
