0bfd140a3240e1efcdb8daa012e612eb7e7303f0
Max Mustermann <maxmustermannbsp@gmail.com>
2016-07-01 23:50:31 +0200

    Add ClipboardTile (2/2)
    Change-Id: I7d7fdbbbaeca27a3c982bf5b1a22a7847fde2ac8

--- sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ sdk/src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -55,6 +55,7 @@ public class QSConstants {
     public static final String TILE_EDIT = "edit";
     public static final String TILE_DND = "dnd";
     public static final String TILE_NIGHTMODE = "night_mode";
+    public static final String TILE_CLIPBOARD = "clipboard";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -102,6 +103,7 @@ public class QSConstants {
         STATIC_TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
         STATIC_TILES_AVAILABLE.add(TILE_NIGHTMODE);
         STATIC_TILES_AVAILABLE.add(TILE_CAFFEINE);
+        STATIC_TILES_AVAILABLE.add(TILE_CLIPBOARD);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
